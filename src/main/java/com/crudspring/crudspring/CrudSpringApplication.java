0a0058cc35779523e71c6245b8bb3b005e947acf
edsoncarlosdvp <edsoncarlos@ymail.com>
2022-02-07 23:39:48 -0300

    Finalizando esta etapa no back

--- src/main/java/com/crudspring/crudspring/CrudSpringApplication.java
+++ src/main/java/com/crudspring/crudspring/CrudSpringApplication.java
@@ -1,13 +1,30 @@
 package com.crudspring.crudspring;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import repository.CourseRepository;
 
 @SpringBootApplication
 public class CrudSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudSpringApplication.class, args);
+	}
+	
+	@Bean
+	CommandLineRunner initDataBase(CourseRepository courseRepository) {
+		return args -> {
+			courseRepository.deleteAll();
+			
+			Course c = new Course();
+			c.setName("Angular com Spring");
+			c.setCategory("Front-End");
+			
+			courseRepository.save(c);
+		}
 	}
 
 }
--- src/main/java/controller/CourseController.java
+++ src/main/java/controller/CourseController.java
@@ -6,15 +6,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.AllArgsConstructor;
 import model.Courses;
+import repository.CourseRepository;
 
 @RestController
 @RequestMapping("/api/courses")
+@AllArgsConstructor
 public class CourseController {
+	
+	private final CourseRepository courseRepository;
 		
-		@GetMapping
-		public List<Courses> list() {
-			return null;
-		}
+	@GetMapping
+	public List<Course> list() {
+		return courseRepository.findAll();
 	}
 }
